43f9f6265b8b0d22292900e0663f5e1ec1a2c03d
Nambiappan <tnambiappan@gmail.com>
2021-05-30 10:28:26 +0530

    Updated requirements file

--- quiz_app_cdk/quiz_app_cdk_stack.py
+++ quiz_app_cdk/quiz_app_cdk_stack.py
@@ -1,15 +1,68 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+from aws_cdk import aws_ec2 as _ec2
+from aws_cdk import aws_ecs as _ecs
+from aws_cdk import aws_ecs_patterns as _ecs_patterns
 
 
-class QuizAppCdkStack(cdk.Stack):
+class QuizAppCdkStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, ** kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # The code that defines your stack goes here):
+        # Create VPC
+        vpc = _ec2.Vpc(
+            self,
+            "chatAppVpc",
+            max_azs=2,
+            nat_gateways=1
+        )
+
+        # Create Fargate Cluster
+        chat_app_cluster = _ecs.Cluster(
+            self,
+            "chatAppCluster"
+        )
+
+        # Create chat service as Fargate Task
+        chat_app_task_def = _ecs.FargateTaskDefinition(
+            self,
+            "chatAppTaskDef"
+        )
+
+        # Create Container Definition
+        chat_app_container = chat_app_task_def.add_container(
+            "chatAppContainer",
+            image=_ecs.ContainerImage.from_registry(
+                "mystique/fargate-chat-app:latest"
+            ),
+            environment={
+                "github": "https://github.com/miztiik"
+            }
+        )
+
+        # Add Port Mapping to Container, Chat app runs on Port 3000
+        chat_app_container.add_port_mappings(
+            _ecs.PortMapping(container_port=3000, protocol=_ecs.Protocol.TCP)
+        )
+
+        # Deploy Container in the micro Service & Attach a LoadBalancer
+        chat_app_service = _ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "Service",
+            cluster=chat_app_cluster,
+            task_definition=chat_app_task_def,
+            assign_public_ip=False,
+            public_load_balancer=True,
+            listener_port=80,
+            desired_count=1,
+            service_name="ChatApp"
+        )
+
+        # Output Chat App Url
+        output_1 = core.CfnOutput(
+            self,
+            "chatAppServiceUrl",
+            value=f"http://{chat_app_service.load_balancer.load_balancer_dns_name}",
+            description="Access the chat app url from your browser"
+        )
